838bcd5323fcb2d40e9fa8643fce85ef9d0b205c
Bonga Dlamini <bongad@sahomeloans.com>
2019-10-28 08:47:08 +0200

    got folder creation working properly

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // Set up dependencies
 
 var _ = require('lodash');
-var fs = require('fs-extra');
+var fs = require('fs');
 var path = require('path');
 var YAML = require('js-yaml');
 var nunjucks = require('nunjucks');
@@ -13,24 +13,37 @@ var outputDir = path.join(__dirname, './output');
 
 /* Check to see if the  output folders exist and create them if they don't. */
 
-function createFolders () {
+function createSubFolders () {
   let folders = YAML.safeLoad(fs.readFileSync(path.join(__dirname, 'sites.yml'), 'utf8'));
   var length = folders.sites.length;
   for (var i = 0; i < length; i++) {
     var foldername = folders.sites[i].name;
-    var folderexist = fs.existsSync(path.join(outputDir, foldername));
+    var folderExist = fs.existsSync(path.join(outputDir, foldername));
     var directory = path.join(outputDir, foldername);
     var fileNameOut = folders.sites[i].name + folders.sites[i].extention;
-    if (folderexist === false) {
+    if (typeof folderExist === 'boolean' && folderExist === false) {
       fs.mkdir(directory, { recursive: true }, (err) => {
         if (err) throw err;
       });
     } else {
       console.log('The directory ' + directory + ' already exists');
-      renderTemplate(directory);
     }
     renderTemplate(directory, fileNameOut);
   }
+}
+
+function createOutDir() {
+
+var folderExist = fs.existsSync(outputDir);
+
+    if (typeof folderExist === 'boolean' && folderExist === false) {
+      fs.mkdir(outputDir, { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      console.log('The directory ' + outputDir + ' already exists');
+    }
+
 }
 
 /* Function that creates the template */
@@ -42,5 +55,5 @@ function renderTemplate (outputDir, fileNameOut) {
   let nujucksOutput = nunjucksEnv.renderString(Templatefile, values);
   fs.writeFileSync(outputFile, nujucksOutput);
 }
-
-createFolders();
+createOutDir();
+createSubFolders();
